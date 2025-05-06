306f7683a5bcda17f41fd6ecd2dc3495891dc697
Gowtham-reddy123 <reddyg026@gmail.com>
2025-05-06 18:46:49 +0530

    task#13

---
+++ task.py
@@ -0,0 +1,19 @@
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+d=int(input("enter the value:"))
+e=int(input("enter the value:"))
+if a>b and a>c and a>d and a>e: 
+	largest_num=a
+elif b>a and b>c and b>d and b>e:
+	largest_num=b
+elif c>a and c>b and c>d and c>e:
+	largest_num=c
+elif d>a and d>b and d>c and d>e:
+	largest_num=d
+else:
+	largert_num=e
+print("the largest value is :",largest_num)
+
+
+	                                                                                             
